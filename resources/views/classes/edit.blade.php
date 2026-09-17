25e93329855a8b6e13d090419e15f7b758780389
Agustino6363 <selvinagustino248@gmail.com>
2026-09-17 20:21:59 +0700

    feat: implement classes

--- resources/views/classes/edit.blade.php
+++ resources/views/classes/edit.blade.php
@@ -23,8 +23,12 @@
     <div>
         <label for="grade"
             class="mb-1.5 block text-xs font-semibold uppercase tracking-[0.1em] text-[#16213A]">Tingkat</label>
-        <input type="text" id="grade" name="grade" value="X/XI/XII"
+        <select id="grade" name="grade"
             class="w-full border border-[#D9D6CD] bg-[#FCFBF8] px-3.5 py-2.5 text-sm focus:border-[#A16207] focus:bg-white focus:outline-none">
+            <option value="X">X</option>
+            <option value="XI">XI</option>
+            <option value="XII" selected>XII</option>
+        </select>
     </div>
 
     <div>
